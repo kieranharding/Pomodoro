cd60a5d1e19bfed3bc16a83915696c603577c1ff
Kieran Harding <kieranharding@gmail.com>
2016-09-22 21:26:02 -0600

    FreshState working
    On entering fresh state, reset the clock to full time
    and ensure the button says "Start". Also, cut down to a
    single button that will alternate between "Start" and "Reset"
    
     On branch master

--- client.js
+++ client.js
@@ -1,65 +1,102 @@
 //  Setup a super state object
 var State = {
+  //  Universal methods
   init: function(target) {
     this.target = target
   },
-  pushButton: function() {},
-  entry: function() {},
-  exit: function() {},
   setEndTime: function(interval) {
-    this.endTime = new Date().getTime() + interval
+    //  Expects interval in milliseconds
+    /*  Storing endTime in clock rather than state so that it will persist if
+        I decide to go soft and allow pausing of pomodoro intervals */
+    this.target.endTime = new Date().getTime() + interval
   },
   getRemainingTime: function() {
-    //  Calculate the time remaining and return it as a formatted string
-    if (this.endTime) {
-      var seconds = ( this.endTime - new Date().getTime() ) / 1000
+    //  Calculate the time remaining and return it in milliseconds
+    /*  Separate from display so that it can be used in calculating the colour
+        gradient. */
+    if (this.target.endTime) {
+      return this.target.endTime - new Date().getTime()
+    }
+  },
+  displayRemainingTime: function() {
+    if (this.target.endTime) {
+      var seconds = Math.floor(getRemainingTime() / 1000)
       var minutes = Math.floor(seconds / 60)
       seconds -= minutes * 60
-      return minutes + ':' + padDigits(seconds)
+      this.target.clock.innerText = minutes + ':' + padDigits(seconds)
     }
+  },
+  setInterval: function(elementName) {
+    var min = $el[elementName].textContent || $el[elementName].innerText
+    this.target.interval = min * 60 * 1000
+  },
+  getInterval: function() {
+    //  Return the total work or rest time in milliseconds.
+    return this.target.interval
   }
 }
 
 //  Create each possible state and override methods
-var StateFresh = createObject(State, {
-  pushButton: function() {
-    this.target.changeState(StateWorking)
+var StateFresh = Object.create(State, {
+  //  StateFresh - page load or after the reset button
+  pushButton: {
+    value: function() {
+      this.target.changeState(StateWorking)
+    },
+    enumerable: false
   },
-  entry: function() {
-
+  entry: {
+    value: function() {
+      this.target.endTime = null  //  Make sure no Remaining Time methods fire
+      this.target.display.innerText = this.target.work.value + ':00'
+      this.target.button.innerText = "Start"
+    },
+    enumerable: false,
+    writable: false,
+    configurable: false
+  },
+  exit: {
+    value: function() {},
+    enumerable: false
   }
-  //    fresh - which is a new one or after the reset button
   //    working - timer is counting the work interval
   //    resting - timer is counting the rest interval
   //    stopped - user has paused the clock. Skip this. Pomodoro or don't.
-
 })
+
 //  Create a clock object
 var clock = {
   init: function() {
+    /* TODO: Store states in an object/list (in here?) so that init doesn't
+    need to know if new ones are added. */
+
+    //  Get some helpers
     this.display = document.getElementById('clock')
     this.work = document.getElementById('work')
     this.rest = document.getElementById('rest')
-  }
-  
-  changeState: function(state) {}
+    this.button = document.getElementById('start-stop')
+    //  Define initial state
+    this.workMinutes = 20
+    this.restMinutes = 10
+    this.state = null
+    //  Create state objects
+    StateFresh.init(this)
+    //  Start with a fresh Pomodoro
+    this.changeState(StateFresh)
+  },
+  changeState: function(state) {
+    this.state && this.state.exit()
+    this.state = state
+    this.state.entry()
+  },
 }
 
-function startTimer(interval) {
-
-  var endTime = new Date().getTime() + interval
-}
-
-function userMilliseconds(elementName) {
-  var min = $el[elementName].textContent || $el[elementName].innerText
-  return min * 60 * 1000
-}
-
-function padDigits(s) {
+function padDigits(s) { //  Where to keep a lonely helper function?
   return s < 10 ? '0' + s : s
 }
 
 window.onload = function() {
+  clock.init()
   // $el.clock = document.getElementById('clock')
   // $el.work = document.getElementById('work')
   // $el.rest = document.getElementById('rest')
